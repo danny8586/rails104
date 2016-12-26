4bb4b8e7d172e9aec6abadaa64a177f059089f58
danny8586 <119074988@qq.com>
2016-12-26 14:14:02 +0800

    create groups index

--- app/helpers/flashes_helper.rb
+++ app/helpers/flashes_helper.rb
@@ -1,9 +1,11 @@
-module FlashesHepler
-	FLASH_CLASSES = { alert: "danger", notice: "success", warning: "warning"}.freeze
-	def flash_class(key)
-		FLASH_CLASSES.fetch key.to_sym, key
-	end
-	def user_facing_flashes
-		flash.to_hash.sline "alert", "notice","warning"
-	end
+module FlashesHelper
+  FLASH_CLASSES = { alert: "danger", notice: "success", warning: "warning"}.freeze
+
+  def flash_class(key)
+    FLASH_CLASSES.fetch key.to_sym, key
+  end
+
+  def user_facing_flashes
+    flash.to_hash.slice "alert", "notice","warning" 
+  end
 end
